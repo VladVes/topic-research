3e800cc9b4877c548c90b0828398ef1b9da3ef8c
VladVes <veselov.workflow@gmail.com>
2023-01-31 17:23:52 +0700

    add simple composition example

--- Patterns/inheritanceAndComposition.ts
+++ Patterns/inheritanceAndComposition.ts
@@ -1,6 +1,6 @@
 // -------------- Inheritance ------------------
 
-abstract class Rectangle {
+abstract class IRectangle {
 	width: number = 0;
 	height: number = 0;
 	area(): number {
@@ -8,30 +8,75 @@ abstract class Rectangle {
 	}
 }
 
-class RectangleWindow extends Rectangle {
+class IRectangleWindow extends IRectangle {
 	open(): boolean {
 		return true;
 	}
 	toggleOpen(): void {}
 } 
 
-class Wall extends Rectangle {
+class Wall extends IRectangle {
 	color: string = 'green';
 } 
 
-// but... class CircleWindow cannot extends Rectangle!
-// so we need to define new parent class
-abstract class Circle {
+// but... what if we need class CircleWindow? CircleWindow cannot extends Rectangle!
+// so we need to define new parent class...
+abstract class ICircle {
 	radius: number = 0;
 	area(): number {
 		return Math.PI * this.radius ** 2;
 	}
 }
 
-class CircleWindow {
-	// duplicate RectangleWindow:
+class ICircleWindow extends ICircle {
+	// ...and duplicate methods RectangleWindow:
 	open(): boolean {
 		return true;
 	}
 	toggleOpen(): void {}
 }
+
+
+// -------------- Composition ------------------
+
+class CWindow {
+	constructor (private dimensions: Shape) {}
+	open(): boolean {
+		return true;
+	}
+	toggleOpen(): void {}
+	area(): number {
+		return this.dimensions.area();
+	}
+}
+
+class CWall {
+	constructor (private dimensions: Shape) {}
+	color: string = 'white';
+	area(): number {
+		return this.dimensions.area();
+	}
+}
+
+interface Shape {
+	area(): number;
+}
+
+// specific fields and methods
+class CRectangle implements Shape {
+	width: number = 10;
+	height: number = 10;
+	area(): number {
+		return this.width * this.height;
+	} 	
+}
+
+class CCircle implements Shape {
+	radius: number = 10;
+	area(): number {
+		return Math.PI * this.radius ** 2;
+	} 	
+}
+
+const rectangleWindow = new CWindow(new CRectangle);
+const circleWindow = new CWindow(new CCircle);
