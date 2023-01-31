3ae89bfd4c94be328dc1b6c1184a7db4786d54dc
VladVes <veselov.workflow@gmail.com>
2023-01-31 16:55:56 +0700

    update inheritance example

--- Patterns/inheritanceAndComposition.ts
+++ Patterns/inheritanceAndComposition.ts
@@ -12,8 +12,26 @@ class RectangleWindow extends Rectangle {
 	open(): boolean {
 		return true;
 	}
+	toggleOpen(): void {}
 } 
 
 class Wall extends Rectangle {
 	color: string = 'green';
 } 
+
+// but... class CircleWindow cannot extends Rectangle!
+// so we need to define new parent class
+abstract class Circle {
+	radius: number = 0;
+	area(): number {
+		return Math.PI * this.radius ** 2;
+	}
+}
+
+class CircleWindow {
+	// duplicate RectangleWindow:
+	open(): boolean {
+		return true;
+	}
+	toggleOpen(): void {}
+}
